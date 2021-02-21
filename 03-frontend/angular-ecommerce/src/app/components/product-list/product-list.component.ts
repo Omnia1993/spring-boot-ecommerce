dbee52398f00e6ef3541ce4a3984354e1519bf37
omnia266 <omniaeid266@gmail.com>
2021-02-21 17:01:11 -0600

    add a small comment

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -9,14 +10,32 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
   products: Product[];
-  constructor(private productService: ProductService) { }
+  currentCategoryId: number;
 
-  ngOnInit(){
-    this.listProducts();    //  this is what was missing
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts()
+    });
   }
   listProducts() {
-    this.productService.getProductList().subscribe(
+    //check to see if id param is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      //get the "id param string . convert string to a number usin the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else{
+      // not category id available ....defult to category 1
+      this.currentCategoryId=1;
+    }
+    //now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
     )
+  }
+}
