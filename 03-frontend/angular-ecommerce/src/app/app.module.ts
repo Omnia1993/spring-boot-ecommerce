42349c58f7ec627a90d36793239c09e94d3579f0
omnia266 <omniaeid266@gmail.com>
2021-03-11 05:35:15 -0600

    update

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -9,6 +9,7 @@ import {Routes ,RouterModule} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
  const routes: Routes=[
    {path:'products/:id',component:ProductDetailsComponent},
@@ -31,11 +32,13 @@ import { ProductDetailsComponent } from './components/product-details/product-de
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent
+    
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
     
   ],
   providers: [ProductService],
