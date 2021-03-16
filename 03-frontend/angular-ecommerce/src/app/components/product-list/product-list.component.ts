56c16be9854c9c1bc6c9f7dcd1543455d84f8efe
omnia266 <omniaeid266@gmail.com>
2021-03-16 08:57:42 -0500

    .

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -21,7 +23,8 @@ export class ProductListComponent implements OnInit {
   perviousKeyWord = null;
 
   constructor(private productService: ProductService,
-    private route: ActivatedRoute) { }
+                      private route: ActivatedRoute,
+                      private cartService:CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -114,6 +117,9 @@ export class ProductListComponent implements OnInit {
     console.log(`Adding to cart :${theProduct.name}, ${theProduct.unitPrice}`);
     
     //TODO ---do real Work
+    
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem)
   }
 
 }
