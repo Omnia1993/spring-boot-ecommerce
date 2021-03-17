3f452d9815c2c308376e5a81252bcf0a70504fab
omnia266 <omniaeid266@gmail.com>
2021-03-17 00:00:59 -0500

    add component  cartDetails

--- 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-details',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-details.component.css']
 })
 export class CartDetailsComponent implements OnInit {
-
-  constructor() { }
+  cartItems: CartItem[];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  constructor(private cartService:CartService) { }
 
   ngOnInit(): void {
+    this.listCartDetails();
+  }
+  listCartDetails() {
+    // get a handle to the cart  items
+    this.cartItems = this.cartService.cartItems;
+
+    // suscribe to the cart totalPrice
+
+
+    // subscribe to the  cart totalQuantity
+
+    // compute cart total price and quantity
   }
 
 }
